f3a84369206dc037830974648b550bc4f394e04f
yagox <yago.ramiresx@gmail.com>
2022-06-30 14:56:08 -0300

    initial config

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../assets/logo-5.png';
+
+import './header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={Logo} alt='' />
+    </header>
+  );
+}
